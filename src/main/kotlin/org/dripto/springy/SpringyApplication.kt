93224c05a69a7c065e85992da9a34d73410d477e
Driptaroop Das <driptaroop.das@gmail.com>
2020-05-14 11:06:01 +0200

    init commit

---
+++ src/main/kotlin/org/dripto/springy/SpringyApplication.kt
@@ -0,0 +1,11 @@
+package org.dripto.springy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringyApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringyApplication>(*args)
+}
---
+++ src/test/kotlin/org/dripto/springy/SpringyApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.dripto.springy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
